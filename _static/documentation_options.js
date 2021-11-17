d9af70c1255afa65da5366d483f1d879e79e2a50
Duc Le <duc.le@stfc.ac.uk>
2021-11-17 06:47:50 +0000

    v3.6.1.ad3b5130b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.6.1.e342142ef',
+    VERSION: '3.6.1.ad3b5130b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
